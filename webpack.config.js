05367b26a22336862c603cd1759e9c47e8c60cc5
James Verch <verchjames@gmail.com>
2018-09-14 13:18:47 -0700

    Changed implementation of GET request to bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: path.join(__dirname, '/client/src/index.jsx'),
   output: {
-    path: path.join(__dirname, '/client/dist/rooms'),
+    path: path.join(__dirname, '/client/dist'),
     filename: 'bundle.js',
   },
   module: {
