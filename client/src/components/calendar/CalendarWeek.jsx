34e1ce0d871d797861a50657106b307eedbce289
James Verch <verchjames@gmail.com>
2018-09-19 16:27:20 -0700

    Implemented working calendar, finalized most CSS

--- client/src/components/calendar/CalendarWeek.jsx
+++ client/src/components/calendar/CalendarWeek.jsx
@@ -5,9 +5,10 @@ const PropTypes = require('prop-types');
 
 function CalendarWeek(props) {
   const { week } = props;
+  console.log('CURR WEEK', week);
   return (
     <tr>
-      {week.map(date => <CalendarDay date={date} />)}
+      {week.map((date, index) => <CalendarDay key={index} date={date} />)}
     </tr>
   );
 }
